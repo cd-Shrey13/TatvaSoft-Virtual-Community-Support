ce85e59deba00165d589d0211d6ca4cdbc0ba477
ShreyP. <shreyprajapati13@gmail.com>
2025-07-03 16:03:08 +0530

    Implement Day 2 assignment

---
+++ Angular (Day-2)/src/form/form.component.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.css'
+
+})
+export class FormComponent {
+
+}
