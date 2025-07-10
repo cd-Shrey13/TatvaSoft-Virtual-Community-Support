ce44b96973455edd7ea3aef8b1cefa373fb96cd5
ShreyP. <shreyprajapati13@gmail.com>
2025-07-10 19:52:27 +0530

    Mission Skill CRUD and Mission Listing User Side

---
+++ Backend/Mission.Entities/context/MissionDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Mission.Entities.context;
+
+public class MissionDbContextFactory : IDesignTimeDbContextFactory<MissionDbContext>
+{
+    public MissionDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<MissionDbContext>();
+
+        // Update this connection string if needed
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=Mission;Username=postgres;Password=admin123");
+
+        return new MissionDbContext(optionsBuilder.Options);
+    }
+}
